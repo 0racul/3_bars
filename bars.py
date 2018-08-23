f0643d2abcd0befce971d3d5ed442d8b1b2d75ce
0racul <nightcrowl@yandex.ru>
2018-08-23 21:52:46 +0700

    21 august remarks fix (PEP8, variables, commit refinement)

--- bars.py
+++ bars.py
@@ -4,8 +4,9 @@ import math
 
 def load_data(filepath):
     with open(filepath, 'r') as f:
-        data = json.loads(f.read())
-    return data
+        data_from_json = json.loads(f.read())
+    return data_from_json
+
 
 def biggest_bar(loaded_bars):
 
@@ -13,17 +14,17 @@ def biggest_bar(loaded_bars):
     bar_seats = []
     max_seat = 0
 
-    for i in range(len(loaded_bars)):
+    for cut_bar_number in range(len(loaded_bars)):
 
-        bar = loaded_bars[i]
+        bar = loaded_bars[cut_bar_number]
         bar_seats.append(bar.get("SeatsCount"))
         max_seat = max(bar_seats)
         bar_names.append(bar.get("Name"))
 
     cut_bars = list(zip(bar_names, bar_seats))
 
-    for i in range(len(cut_bars)):
-        cut_bar = cut_bars[i]
+    for cut_bar_number in range(len(cut_bars)):
+        cut_bar = cut_bars[cut_bar_number]
         if cut_bar[1] == max_seat:
             return cut_bar
         else:
@@ -36,23 +37,22 @@ def smallest_bar(loaded_bars):
     bar_seats = []
     min_seat = 0
 
-    for i in range(len(loaded_bars)):
+    for loaded_bar_number in range(len(loaded_bars)):
 
-        bar = loaded_bars[i]
+        bar = loaded_bars[loaded_bar_number]
         bar_seats.append(bar.get("SeatsCount"))
         min_seat = min(bar_seats)
         bar_names.append(bar.get("Name"))
 
     cut_bars = list(zip(bar_names, bar_seats))
 
-    for i in range(len(cut_bars)):
+    for cut_bar_number in range(len(cut_bars)):
 
-        cut_bar = cut_bars[i]
+        cut_bar = cut_bars[cut_bar_number]
         if cut_bar[1] == min_seat:
             return cut_bar
         else:
             continue
-
 
 
 def find_bar(loaded_bars, current_longitude, current_latitude):
@@ -60,15 +60,21 @@ def find_bar(loaded_bars, current_longitude, current_latitude):
     bars_with_distances = []
     distances = []
 
-    for i in range(len(loaded_bars)):
+    for bar_number in range(len(loaded_bars)):
 
-        current_bar = loaded_bars[i]
+        current_bar = loaded_bars[bar_number]
         bar_latitude = float(current_bar["Latitude_WGS84"])
         bar_longitude = float(current_bar["Longitude_WGS84"])
-        distance_to_bar = math.sqrt((bar_latitude - current_latitude)**2 + (bar_longitude - current_longitude)**2)
-        bars_with_distances.append({"Name": current_bar["Name"], "Latitude": bar_latitude, "Longitude": bar_longitude, "Address":current_bar["Address"], "Distance":distance_to_bar})
-        distances.append(bars_with_distances[i]["Distance"])
 
+        distance_to_bar = math.sqrt((bar_latitude - current_latitude)**2 +
+                                    (bar_longitude - current_longitude)**2)
+
+        bars_with_distances.append({"Name": current_bar["Name"],
+                                    "Latitude": bar_latitude,
+                                    "Longitude": bar_longitude,
+                                    "Address": current_bar["Address"],
+                                    "Distance": distance_to_bar})
+        distances.append(bars_with_distances[i]["Distance"])
 
     min_distance = min(distances)
     needed_bar_index = distances.index(min_distance)
@@ -96,8 +102,6 @@ if __name__ == '__main__':
     biggest = biggest_bar(loaded_bars)
     smallest = smallest_bar(loaded_bars)
 
-    print("Biggest bar in Moscow  " + str(biggest) + "\nSmallest bar in Moscow  " + str(smallest) + "\nNearest bar to your location " + str(find))
-
-
-#ayamodiskodancer#
-
+    print("Biggest bar in Moscow " + str(biggest))
+    print("Smallest bar " + str(smallest))
+    print("Nearest bar to your loc " + str(find))
